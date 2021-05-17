3ef33097e794e46cdeb8cf7d4d345cef85b4a746
Thanigavel Pannerselvam <thanigavel@live.com>
2021-05-17 12:42:50 +0100

    Updating response codes

---
+++ src/AFI.API.Core.Tests/Services/RegistrationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AFI.API.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFI.API.Core.Services.Tests
+{
+    [TestClass()]
+    public class RegistrationServiceTests
+    {
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
