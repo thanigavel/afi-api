a808b3e8e8054a161e41e7f74fc916b605521e94
Thanigavel Pannerselvam <thanigavel@live.com>
2021-05-17 09:43:32 +0100

    Implementing Field validations

---
+++ src/AFI.API.Common/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace AFI.API.Common.Constants
+{
+    public static class Constants
+    {
+        public const int NAMEMINLENGTH = 3;
+        public const int NAMEMAXLENGTH = 50;
+        public const string POLICYREFERENCEFORMAT = @"^[A-Z]{2}-\d{6}$";
+        public const int MINIMUMAGEINYEARS = 18;
+        public const string EMAILADDRESSFORMAT = @"^(?i)[a-zA-Z0-9]{4,}@[a-zA-Z0-9]{2,}(?:.com|.co.uk)$";
+    }
+}
--- src/AFI.API.Model/Customer.cs
+++ src/AFI.API.Model/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AFI.API.Common.Constants;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace AFI.API.Model
@@ -6,15 +7,21 @@ namespace AFI.API.Model
     public class Customer
     {
         [Required]
+        [StringLength(Constants.NAMEMAXLENGTH, MinimumLength = Constants.NAMEMINLENGTH)]
         public string FirstName { get; set; }
+
         [Required]
+        [StringLength(Constants.NAMEMAXLENGTH, MinimumLength = Constants.NAMEMINLENGTH)]
         public string Surname { get; set; }
+
         [Required]
+        [RegularExpression(Constants.POLICYREFERENCEFORMAT, ErrorMessage = "Please provide the correct policy reference number.")]
         public string PolicyReference { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime DoB { get; set; }
 
+        [RegularExpression(Constants.EMAILADDRESSFORMAT)]
         public string EmailAddress { get; set; }
     }
 }
